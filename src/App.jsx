2cec68d724438fcf8032471897a063c8112814b0
Ogabek <150121693+ogabekrustamov@users.noreply.github.com>
2025-07-01 15:49:59 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <img  className="flex relative align-middle w-[300px]" src="/src/assets/illustration.svg" alt="Illustration" />
+    </>
+  )
+}
+
+export default App
