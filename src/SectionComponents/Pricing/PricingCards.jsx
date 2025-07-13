789e7d790125dc9cb347010c6b4ca5be94c1c65b
Ogabek <150121693+ogabekrustamov@users.noreply.github.com>
2025-07-13 20:14:09 +0500

    price section completion

--- src/SectionComponents/Pricing/PricingCards.jsx
+++ src/SectionComponents/Pricing/PricingCards.jsx
@@ -7,15 +7,15 @@ const PricingCards = () => {
             <div className={`w-[90vw] max-w-[1240px]`}>
                 <div>
                     <h1 className={`text-[75px] font-semibold `}>Pricing</h1>
-                    <p className={`text-[16px] `}>Elevate your Online Precense.Competetive pricing fro Exceprional results</p>
+                    <p className={`text-[18px] `}>Elevate your Online Precense.Competetive pricing <br/> fro Exceprional results</p>
                 </div>
-                <div className={`flex`}>
-                    <div className={`w-[350px] max-h-[950px] border-[1px] border-black rounded-[35px] flex flex-col p-[25px] border-b-[6px]`}>
+                <div className={`flex justify-between mt-[60px] gap-[20px] max-lg:grid max-lg:grid-cols-2 max-sm:flex max-sm:flex-col`}>
+                    <div className={`w-[370px] max-h-[950px] border-[1px] max-lg:w-full max-sm:p-[35px] border-black rounded-[35px] flex flex-col p-[25px] border-b-[6px]`}>
                          <div className="mb-[30px]">
                              <h3 className={`text-[25px] font-semibold`}>Basic Plan</h3>
                              <h2 className={`text-[40px] font-[600]`}>$500 <span className={`text-[16px] font-[300]`}>\month</span></h2>
                          </div>
-                        <button className={`w-full h-[60px] bg-black text-white cursor-pointer rounded-[15px] mb-[20px]`}>Get started</button>
+                        <button className={`w-full h-[60px] bg-[rgb(25,26,35)] text-white cursor-pointer rounded-[15px] mb-[20px]`}>Get started</button>
                         <button className={`w-full h-[60px]  text-black cursor-pointer border-[2px] border-black rounded-[15px] mb-[30px]`}>Request a</button>
                         <div className={`w-full bg-black h-[2px] `}></div>
                         <div className={`w-full flex flex-col`}>
@@ -51,6 +51,93 @@ const PricingCards = () => {
                             </div>
                         </div>
                     </div>
+                    <div className={`w-[370px] max-h-[950px] border-[1px] max-lg:w-full max-sm:p-[35px] bg-[rgb(25,26,35)] border-[rgb(25,26,35)] text-white rounded-[35px] flex flex-col p-[25px] border-b-[6px]`}>
+                        <div className="mb-[30px]  flex">
+
+                            <div>
+                                <h3 className={`text-[25px] font-semibold`}>Pro Plan</h3>
+                                <h2 className={`text-[40px] font-[600]`}>$1000 <span className={`text-[16px] font-[300]`}>\month</span></h2>
+                            </div>
+                            <div className={`w-[100px] h-[30px] rounded-[15px] bg-[rgb(207,255,113)] flex justify-center items-center text-black`}>Popular</div>
+                        </div>
+                        <button className={`w-full h-[60px] bg-[rgb(207,255,113)] text-black cursor-pointer rounded-[15px] mb-[20px]`}>Get started</button>
+                        <button className={`w-full h-[60px]  text-white cursor-pointer border-[2px] border-white rounded-[15px] mb-[30px]`}>Request a quote</button>
+                        <div className={`w-full bg-white h-[2px] `}></div>
+                        <div className={`w-full flex flex-col `}>
+                            <div className={`flex gap-[20px] mt-[30px]`}>
+                                <div className="w-8 h-8 bg-[rgb(207,255,113)] rounded-full flex items-center justify-center">
+                                    <div className={`fas fa-soli fa-check text-black`}></div>
+                                </div>
+                                <p>Website optimization</p>
+                            </div>
+                            <div className={`flex gap-[20px] mt-[30px]`}>
+                                <div className="w-8 h-8 bg-[rgb(207,255,113)] rounded-full flex items-center justify-center">
+                                    <div className={`fas fa-soli fa-check text-black`}></div>
+                                </div>
+                                <p>Website optimization</p>
+                            </div>
+                            <div className={`flex gap-[20px] mt-[30px]`}>
+                                <div className="w-8 h-8 bg-[rgb(207,255,113)] rounded-full flex items-center justify-center">
+                                    <div className={`fas fa-soli fa-check text-black`}></div>
+                                </div>
+                                <p>Website optimization</p>
+                            </div>
+                            <div className={`flex gap-[20px] mt-[30px]`}>
+                                <div className="w-8 h-8 bg-[rgb(207,255,113)] rounded-full flex items-center justify-center">
+                                    <div className={`fas fa-soli fa-check text-black`}></div>
+                                </div>
+                                <p>Website optimization</p>
+                            </div>
+                            <div className={`flex gap-[20px] mt-[30px]`}>
+                                <div className="w-8 h-8 bg-[rgb(207,255,113)] rounded-full flex items-center justify-center">
+                                    <div className={`fas fa-soli fa-check text-black`}></div>
+                                </div>
+                                <p>Website optimization</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={`w-[370px] max-h-[950px] border-[1px] max-lg:w-full max-sm:p-[35px] border-black rounded-[35px] flex flex-col p-[25px] border-b-[6px]`}>
+                        <div className="mb-[30px]">
+                            <h3 className={`text-[25px] font-semibold`}>Basic Plan</h3>
+                            <h2 className={`text-[40px] font-[600]`}>$2000 <span className={`text-[16px] font-[300]`}>\month</span></h2>
+                        </div>
+                        <button className={`w-full h-[60px] bg-[rgb(25,26,35)] text-white cursor-pointer rounded-[15px] mb-[20px]`}>Get started</button>
+                        <button className={`w-full h-[60px]  text-black cursor-pointer border-[2px] border-black rounded-[15px] mb-[30px]`}>Request a</button>
+                        <div className={`w-full bg-black h-[2px] `}></div>
+                        <div className={`w-full flex flex-col`}>
+                            <div className={`flex gap-[20px] mt-[30px]`}>
+                                <div className="w-8 h-8 bg-[rgb(207,255,113)] rounded-full flex items-center justify-center">
+                                    <div className={`fas fa-soli fa-check`}></div>
+                                </div>
+                                <p>Website optimization</p>
+                            </div>
+                            <div className={`flex gap-[20px] mt-[30px]`}>
+                                <div className="w-8 h-8 bg-[rgb(207,255,113)] rounded-full flex items-center justify-center">
+                                    <div className={`fas fa-soli fa-check`}></div>
+                                </div>
+                                <p>Website optimization</p>
+                            </div>
+                            <div className={`flex gap-[20px] mt-[30px]`}>
+                                <div className="w-8 h-8 bg-[rgb(207,255,113)] rounded-full flex items-center justify-center">
+                                    <div className={`fas fa-soli fa-check`}></div>
+                                </div>
+                                <p>Website optimization</p>
+                            </div>
+                            <div className={`flex gap-[20px] mt-[30px]`}>
+                                <div className="w-8 h-8 bg-[rgb(207,255,113)] rounded-full flex items-center justify-center">
+                                    <div className={`fas fa-soli fa-check`}></div>
+                                </div>
+                                <p>Website optimization</p>
+                            </div>
+                            <div className={`flex gap-[20px] mt-[30px]`}>
+                                <div className="w-8 h-8 bg-[rgb(207,255,113)] rounded-full flex items-center justify-center">
+                                    <div className={`fas fa-soli fa-check`}></div>
+                                </div>
+                                <p>Website optimization</p>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
